9403ed1fc98856c0cb79ad01a33c0c77a685583d
Daniel Svensson <dasv22@student.bth.se>
2024-04-01 12:50:32 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+MVC 2024
+dasv22
+Gjort av Daniel Svensson
+
+TO clone the repo make an empty folder in your terminal.
+go into the folder and run the following:
+git clone https://github.com/Billyherringtonbeliever86/mvc_save.git
+
+Dwonload the gitrepo and stand in the folder you just created in a terminal then run the following command: php -S localhost:8888 -t public
+
+this repository https://github.com/Billyherringtonbeliever86/mvc_save.git
+![Alt text](./public/img/php-code.jpeg)
+
---
+++ src/Controller/ControllerJSON.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class ControllerJSON extends AbstractController
+{
+    #[Route("api/")]
+    public function apis(): Response
+    {
+        return $this->render('apis.html.twig');
+    }
+
+    #[Route("/api/qoute")]
+    public function jsonQoute(): Response
+    {
+        $number = random_int(0, 2);
+        $qoutes = [
+            "I have the high ground Anakin",
+            "Whats in the car? Seats and Steering Wheel",
+            "Ariel listen to me the human world is a mess, life under the sea is better than anything they got up there"
+        ];
+        $datum = date('Y-m-d H:i:s');
+
+        $qoute = $qoutes[$number];
+        $data = [
+            "Dagens citat" => $qoute,
+            "Dagens datum och tid av genererat svar" => $datum,
+
+        ];
+
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;    }
+}
+
