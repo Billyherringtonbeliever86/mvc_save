14b2104667068e68b6ddabbc2dab1af5f35afb36
Daniel Svensson <dasv22@student.bth.se>
2024-04-15 15:01:46 +0200

    Card hand class created

--- src/Card/CardHand.php
+++ src/Card/CardHand.php
@@ -1,8 +1,36 @@
 <?php
 
+
 namespace App\Card;
+use App\Card\Card;
 
 class CardHand
 {
-    
+    private $hand = [];
+
+    public function add(Card $card): void
+    {
+        $this->hand[] = $card;
+    }
+
+    public function deal(): void
+    {
+        foreach ($this->hand as $card) {
+            $card->deal();
+        }
+    }
+
+    public function getNumberCards(): int
+    {
+        return count($this->hand);
+    }
+
+    public function getCard(): array
+    {
+        $values = [];
+        foreach ($this->hand as $card) {
+            $values[] = $card->getValue();
+        }
+        return $values;
+    }
 }
